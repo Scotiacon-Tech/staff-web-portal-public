251454f01f3f03eb93d87b9089dd6be75140844b
Aaron Parker <github@forester.knightmare.org.uk>
2024-10-12 02:27:36 +0100

    fix: footer height

---
+++ assets/AuthLogout-lQ53MVe1.js
@@ -0,0 +1,1 @@
+import{T as n,F as o,D as s,as as a}from"./index-UZxHwuFp.js";const r=()=>{n.getInstance().removeTokensByService("AuthAPI"),o.getInstance().emit("logout")},m=s({__name:"AuthLogout",setup(t){return r(),a().push("/"),(e,u)=>null}});export{m as default};
